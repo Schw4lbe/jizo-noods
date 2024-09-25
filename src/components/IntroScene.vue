7b3772c7f822972adf426cf8eb38ba9290ff4727
TapeMate <admin@tapemate.onmicrosoft.com>
2024-09-26 00:10:25 +0200

    redesigned navbar.

--- src/components/IntroScene.vue
+++ src/components/IntroScene.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="section-intro-main">
-    <img src="../../public/img/intro/intro-sample.jpg" alt="dummy" />
+    <img
+      class="intro-background"
+      src="../../public/img/intro/intro-sample.jpg"
+      alt="dummy"
+    />
     <img src="../../public/img/logo.png" alt="dummy" />
     <h3 class="intro-slogan">Slogan Text Dummy</h3>
   </section>
--- src/components/utilities/NavbarMain.vue
+++ src/components/utilities/NavbarMain.vue
@@ -1,29 +1,44 @@
 <template>
-  <nav class="navbar navbar-expand-lg">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
     <div class="container-fluid">
       <button
         class="navbar-toggler"
         type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
         aria-label="Toggle navigation"
       >
-        <span class="icon"><i class="fa-sharp fa-solid fa-bars"></i></span>
+        <i class="fa-sharp fa-solid fa-bars navbar-toggler-button"></i>
       </button>
-      <div class="scrolling-wrapper">
-        <div class="marquee">
-          <p>{{ content.navbarMain.scrollingText }}</p>
-          <p>{{ content.navbarMain.scrollingText }}</p>
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li v-for="(item, index) in navItems" :key="index">
+              <a class="nav-item" :href="item.url">{{ item.lable }}</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li v-for="(item, index) in navItems" :key="index">
-            <a class="nav-item" :href="item.url">{{ item.lable }}</a>
-          </li>
-        </ul>
+    </div>
+    <div class="scrolling-wrapper">
+      <div class="marquee">
+        <p>{{ content.navbarMain.scrollingText }}</p>
+        <p>{{ content.navbarMain.scrollingText }}</p>
       </div>
     </div>
   </nav>
