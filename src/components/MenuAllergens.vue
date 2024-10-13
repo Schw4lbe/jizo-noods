cc4277fdff61554f0ab2874d81d147b47f7057af
TapeMate <admin@tapemate.onmicrosoft.com>
2024-10-14 00:07:15 +0200

    added content info to allergens. reworked layout and build for minimal spacing.

--- src/components/MenuAllergens.vue
+++ src/components/MenuAllergens.vue
@@ -1,35 +1,45 @@
 <template>
   <div class="menu-allergen-container">
     <h4 class="allergen-header">{{ allergenContent.header }}</h4>
-    <p class="allergen-description">
+    <!-- <p class="allergen-description">
       {{ allergenContent.description }}
-    </p>
+    </p> -->
     <div class="accordion-wrapper">
       <div class="accordion" id="allergensAccordion">
-        <div
-          v-for="item in allergenContent.items"
-          :key="item.id"
-          class="accordion-item"
-        >
+        <div class="accordion-item">
           <h5 class="accordion-header">
             <button
               class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              :data-bs-target="`#collapse-allergen-${item.id}`"
+              data-bs-target="#collapse-allergens"
               aria-expanded="false"
-              :aria-controls="`#collapse-allergen-${item.id}`"
+              aria-controls="#collapse-allergens"
             >
-              <span class="allergen-id">{{ item.id + ") " + item.label }}</span>
+              <span class="allergen-description">{{
+                allergenContent.description
+              }}</span>
             </button>
           </h5>
           <div
-            :id="`collapse-allergen-${item.id}`"
+            id="collapse-allergens"
             class="accordion-collapse collapse"
             data-bs-parent="#allergensAccordion"
-            style=""
           >
-            <div class="accordion-body">{{ item.description }}</div>
+            <div class="accordion-body">
+              <!-- v-for loop here with all allergens in one info box -->
+              <ul class="allergens-list">
+                <li
+                  v-for="item in allergenContent.items"
+                  :key="item.id"
+                  class="allergen-item"
+                >
+                  <span class="item-id">{{ item.id + ") " }}</span>
+                  <span class="item-label">{{ item.label + ": " }}</span>
+                  <span class="item-description">{{ item.description }}</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
