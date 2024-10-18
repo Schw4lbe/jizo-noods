6c3e3cccf28a92718af25d3c3901be7b20089494
TapeMate <admin@tapemate.onmicrosoft.com>
2024-10-18 12:15:57 +0200

    updated and consolidated products in styling to one single component.

--- src/components/MenuProducts.vue
+++ src/components/MenuProducts.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="menu-product-group-container">
     <div class="product-group-header">{{ productConten.header }}</div>
-    <div class="product-group-header">{{ productConten.teaser }}</div>
-    <div class="group-items-container">
+    <div class="product-group-teaser">{{ productConten.teaser }}</div>
+    <div class="items-container">
       <div
         v-for="(item, index) in productConten.items"
         :key="index"
-        class="product-item"
+        class="item"
       >
         <p class="item-name">{{ item.name }}</p>
         <div class="item-ingredients">
@@ -25,8 +25,9 @@
             v-for="(allItem, allIndex) in item.allergens"
             :key="allIndex"
             class="item-allergen-id"
-            >{{ allItem }}</span
-          >
+            >{{ allItem
+            }}<span v-if="allIndex < item.allergens.length - 1"> , </span>
+          </span>
         </div>
         <div class="item-price">
           <span class="currency">kr </span>
