eaf6f06fddca066dee07ae65aadd15bfec72eb55
TapeMate <admin@tapemate.onmicrosoft.com>
2024-10-23 00:44:54 +0200

    major content and styling update.

--- src/components/SectionAboutUs.vue
+++ src/components/SectionAboutUs.vue
@@ -2,23 +2,38 @@
   <section class="section-about-main" id="section-about-us">
     <h3 class="section-header">{{ sectionContent.header }}</h3>
     <div class="about-content-container">
-      <div
-        v-for="(item, index) in sectionContent.items"
-        :key="index"
-        class="about-item"
-      >
-        <h4 class="content-header">{{ item.header }}</h4>
-        <div class="content-text">
-          <span
-            v-for="(item, index) in item.content"
-            :key="index"
-            class="text"
-            :class="{ highlighted: item.isHighlighted }"
-            >{{ item.text }}</span
-          >
+      <h4 class="content-subheader">{{ sectionContent.subheader }}</h4>
+      <div class="content-grid">
+        <div class="grid-item">
+          <img
+            src="../../public/img/about-us/500x1000_2.png"
+            alt="Jizu Manga Art"
+            class="content-image"
+          />
+          <p class="content-text">
+            <span class="highlight">Jizo Noods</span> is all about celebrating
+            the art of slurpin’ delicious noodle soups from Japan & South Korea.
+            Since 2024 kicked off, we've been cooperating with folks across Oslo
+            like,
+            <span class="highlight">Trekanten Sigurd Ramen & Fuglen Cafe </span
+            >for PopUps, bringing the magic of homemade dishes to the people.
+          </p>
         </div>
-        <div class="media-container">
-          <img :src="item.media" alt="dummy" class="media" />
+
+        <div class="grid-item">
+          <p class="content-text">
+            And now, guess what? We’re hyped to announce that we’re taking it up
+            a notch! Thanks to our friends at Trekanten, we’ll be dishing out
+            our soups and tasty plates
+            <span class="highlight">every Thursday and Friday</span>. So Oslo,
+            get your chopsticks ready - it’s time to dive into noodle heaven.
+            See ya there, slurp fam!
+          </p>
+          <img
+            src="../../public/img/about-us/500x1000_1.png"
+            alt="Jizu Manga Art"
+            class="content-image"
+          />
         </div>
       </div>
     </div>
